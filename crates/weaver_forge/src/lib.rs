2db3a6e5ab738c05c696259fa2172b51e10fba7f
Liudmila Molkova <neskazu@gmail.com>
2025-09-12 08:43:18 -0700

    refactor and clean up registry

--- crates/weaver_forge/src/lib.rs
+++ crates/weaver_forge/src/lib.rs
@@ -700,7 +700,7 @@ mod tests {
     use serde::Serialize;
 
     use weaver_diff::diff_dir;
-        use weaver_resolver::SchemaResolver;
+    use weaver_resolver::SchemaResolver;
     use weaver_semconv::registry::SemConvRegistry;
 
     use crate::config::{ApplicationMode, CaseConvention, Params, TemplateConfig, WeaverConfig};
